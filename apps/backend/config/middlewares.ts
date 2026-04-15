67a48cdc025b7928e633babe14f1a79db17f0816
Bubuh Bukhori <bukhori_@outlook.com>
2026-04-15 16:04:57 +0700

    change

--- apps/backend/config/middlewares.ts
+++ apps/backend/config/middlewares.ts
@@ -1,39 +1,12 @@
-export default ({ env }) => {
-	const awsBucket = env("AWS_BUCKET");
-	const awsRegion = env("AWS_REGION", "ap-southeast-1");
-	const s3AssetsUrl = awsBucket
-		? `${awsBucket}.s3.${awsRegion}.amazonaws.com`
-		: undefined;
-
-	return [
-		"strapi::logger",
-		"strapi::errors",
-		{
-			name: "strapi::security",
-			config: {
-				contentSecurityPolicy: {
-					useDefaults: true,
-					directives: {
-						"connect-src": ["'self'", "https:"],
-						"script-src": ["'self'", "'unsafe-inline'"],
-						"img-src": ["'self'", "data:", "blob:", s3AssetsUrl],
-						"media-src": ["'self'", "data:", "blob:", s3AssetsUrl],
-						upgradeInsecureRequests: null,
-					},
-				},
-			},
-		},
-		{
-			name: "strapi::cors",
-			config: {
-				headers: "*",
-				origin: [env("STRAPI_ALLOWED_ORIGIN", "*")],
-			},
-		},
-		"strapi::query",
-		"strapi::body",
-		"strapi::session",
-		"strapi::favicon",
-		"strapi::public",
-	];
-};
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
